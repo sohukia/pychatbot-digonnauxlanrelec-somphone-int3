6b4b954dad65632e301a0578853739c34fe59c30
Somphone-Isabelle <isabelle.somphone@efrei.net>
2023-11-18 13:07:00 +0100

    better comment and refactor tf func

--- src/tf_idf.py
+++ src/tf_idf.py
@@ -4,10 +4,19 @@ import math
 
 def term_frequency(text: list) -> dict:
     """
-    Use comprehension to create a dictionary with each word as keys and its occurrences as values
+    Take a whole text as a list and analyse each word, 
+    count the number of occurrences of each word and store the final and store everything in a dictionary with 
+    the word as key and its occurrences counter as value
     :param text: str
     :return: term frequency dictionary
     """
+    tf_dict = {}
+    for word in text:
+        if word not in tf_dict.keys():
+            tf_dict[word] = 1
+        else:
+            tf_dict[word] += 1
+    
     return {word: text.count(word) for word in text}
 
 
@@ -20,11 +29,38 @@ def list_document_tf(path: str) -> list[dict]:
     file_list = utils.list_files(path, '.txt')
     documents_tf = []
     for file in file_list:
-        with open(file, 'r') as f:
+        with open(file, 'r', encoding='utf-8') as f:
             content = ' '.join([word.removesuffix('\n') for word in f.readlines()]).split()
             term_frequency_document = term_frequency(content)
         documents_tf.append(term_frequency_document)
     return documents_tf
+
+
+def term_frequency_all(path: str) -> dict:
+    """
+    Compute term frequency across all documents in the given directory
+    :param path: str
+    :return: dictionary of tf across documents.
+    """
+    file_list = utils.list_files(path, '.txt')
+    text_across_documents = []
+
+    for file in file_list:
+        with open(file, 'r', encoding='utf-8') as f:
+            content = ' '.join([word.removesuffix('\n') for word in f.readlines()]).split()
+            text_across_documents += content
+    return term_frequency(text_across_documents)
+
+
+"""
+For each word within the term_frequency all
+Find if it appears at least once in each document frequency (list document frequency) then compute the inverse of this proportion
+(count the number of document)
+Finaly compute the logarithm base 10 of this value and associate it with each word.
+Each word should have at least one occurrence across all files. 
+"""
+
+
 
 
 def inverse_document_frequency(term_freq: dict) -> dict:
