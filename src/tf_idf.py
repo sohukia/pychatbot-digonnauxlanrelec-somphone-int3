0444d14441e155216cbcaf9d52b40abce5c2051b
Somphone-Isabelle <isabelle.somphone@efrei.net>
2023-11-24 14:53:16 +0100

    idf function fixed

--- src/tf_idf.py
+++ src/tf_idf.py
@@ -44,7 +44,6 @@ def term_frequency_all(path: str) -> dict:
     """
     file_list = utils.list_files(path, '.txt')
     text_across_documents = []
-
     for file in file_list:
         with open(file, 'r', encoding='utf-8') as f:
             content = ' '.join([word.removesuffix('\n') for word in f.readlines()]).split()
@@ -52,22 +51,28 @@ def term_frequency_all(path: str) -> dict:
     return term_frequency(text_across_documents)
 
 
-def tf_idf(
-"""
-For each word within the term_frequency all
-Find if it appears at least once in each document frequency (list document frequency) then compute the inverse of this proportion
-(count the number of document)
-Finaly compute the logarithm base 10 of this value and associate it with each word.
-Each word should have at least one occurrence across all files. 
-"""
-
-
-
-
-def inverse_document_frequency(term_freq: dict) -> dict:
+def inverse_document_frequency(term_freq_all: dict) -> dict:
     """
-    Take every value of the term frequency dictionary and compute the logarithm of the inverse of this frequency
-    :param term_freq: dict containing each word frequency
-    :return: inverse document frequency dictionary
+    For each word within the term_frequency_all
+    Find if it appears at least once in each document frequency (list document frequency) then compute the inverse of this proportion
+    (count the number of document)
+    Finaly compute the logarithm base 10 of this value and associate it with each word.
+    Each word should have at least one occurrence across all files. 
     """
-    return {word: math.log10(value) for word, value in term_freq.items()}
+    inv_doc_freq = {}
+    all_texts = []
+    files = utils.list_files('./cleaned', ".txt")
+
+    for file in files:
+        with open(file, 'r', encoding="utf-8") as f:
+            # put all documents into a single list (its size will then be the number of document)
+            all_texts.append(' '.join(map(lambda x: x.removesuffix('\n'),f.readlines())))
+    for word in term_freq_all.keys():
+        counter = 0
+        for text in all_texts:
+            # if the word appears in the text, its proportion is increased by one
+            if word in text:
+                counter += 1
+        # compute the inverse frequency of this word
+        inv_doc_freq[word] = math.log10(1/counter)
+    return inv_doc_freq
