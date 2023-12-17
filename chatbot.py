71d941fcfb1da622e99e96cea04a03158c52a9d1
Somphone-Isabelle <isabelle.somphone@efrei.net>
2023-12-17 14:03:25 +0100

    project finished

--- chatbot.py
+++ chatbot.py
@@ -10,13 +10,7 @@ from source.menu import Menu
 
 
 if __name__ == '__main__':
-    # copy and clear files
-    # file_c.copy_files(utils.list_files('./speeches', '.txt'))
-
     # MAIN
-    # menu = Menu()
-
-    # functions = Functions()
-    # print(functions.most_relevant_document("Peux-tu me dire comment une nation peut-elle prendre soin du climat ?"))
-
     menu = Menu()
+    #import source.utils as utils
+    #utils.Utils.my_input("test")
